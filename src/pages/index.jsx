0ddf5b398d07315ce56aa188795899967186ddb0
Hebert Richard Masseno Dias <hebertdev82@gmail.com>
2022-09-02 19:27:00 -0300

    Updating Project

---
+++ src/pages/index.jsx
@@ -0,0 +1,10 @@
+export default function Index() {
+    return (
+        <div>
+            <center>
+                <h1>Painel Administrativo</h1>
+                <h2>Atualizando...</h2>
+            </center>
+        </div>
+    );
+}
